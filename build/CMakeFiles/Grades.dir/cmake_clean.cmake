60c5562d04d7e56ff8542749fddbb6e8d586519d
andrArs <andreea.arseanu@student.upt.ro>
2025-11-18 23:25:00 +0200

    tema 2

---
+++ build/CMakeFiles/Grades.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grades.dir/src/Grades.cpp.o"
+  "CMakeFiles/Grades.dir/src/Grades.cpp.o.d"
+  "CMakeFiles/Grades.dir/src/main.cpp.o"
+  "CMakeFiles/Grades.dir/src/main.cpp.o.d"
+  "Grades"
+  "Grades.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grades.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
